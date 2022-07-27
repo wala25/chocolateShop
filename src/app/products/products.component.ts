f0d5bd6082f2bb6e314a05fe25b15c8d0866ab1a
Walaeddine <walaeddine.web.dev@gmail.com>
2022-07-27 13:05:17 +0100

    initial commit

---
+++ src/app/products/products.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss']
+})
+export class ProductsComponent implements OnInit {
+
+  constructor() { }
+
+  photos=['photo.png','photo.png','photo.png','photo.png']
+dip=new Array(4).fill(false)
+  disp(i:number){
+    this.dip[i]=!this.dip[i]
+  }
+  ngOnInit(): void {
+  }
+
+}
